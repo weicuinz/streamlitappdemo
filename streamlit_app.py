9f2e3a62d638ebbd67cd247c111898384b1a3842
weicuinz <36883556+weicuinz@users.noreply.github.com>
2022-11-17 10:17:25 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,12 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+# run a snowflake query
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
+
+# put data into a dataframe
+df = pandas.DataFrame(my_catalog)
+
+# write data frame output to the web app.
+streamlit.write(df)
